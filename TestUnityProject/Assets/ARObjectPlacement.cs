cf2a525dc085dfaa1a9af918f6420fc9a3e39c74
Li, Jacob (Agoda) <Jacob.Li@agoda.com>
2021-09-13 12:29:31 -0400

    change version to 3.14f

--- TestUnityProject/Assets/ARObjectPlacement.cs
+++ TestUnityProject/Assets/ARObjectPlacement.cs
@@ -1,9 +1,7 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.ARFoundation;
 using UnityEngine.XR.ARSubsystems;
-using System;
 
 public class ARObjectPlacement : MonoBehaviour
 {
