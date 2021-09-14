8bfcf014996d74d4fdd99b14ba0cfe776ffe2847
Li, Jacob (Agoda) <Jacob.Li@agoda.com>
2021-09-14 18:36:06 -0400

    update object placement and partial moving

--- TestUnityProject/Assets/ARObjectPlacement.cs
+++ TestUnityProject/Assets/ARObjectPlacement.cs
@@ -9,35 +9,99 @@ public class ARObjectPlacement : MonoBehaviour
 {
     public GameObject cabinetToPlace;
     public GameObject shelfToPlace;
+    public GameObject bottle;
+    public GameObject cup;
     public GameObject placementIndicator;
 
     private ARSessionOrigin arOrigin;
     private ARRaycastManager raycastManager;
     private Pose placementPose;
+private Vector2 touchPosition = default;
+
     private bool placementPoseIsValid = false;
+    private bool isBeginningObjectSpawned = false;
+    private bool isTouchHolding = false;
+
+    private float objectMovingSpeed;
+
+    private GameObject placedObject;
 
     void Start()
     {
         //arOrigin = FindObjectOfType<ARSessionOrigin>();
         raycastManager = FindObjectOfType<ARRaycastManager>();
+        objectMovingSpeed = 0.001f;
     }
 
     void Update()
     {
-        UpdatePlacement();
-        UpdatePlacementIndicator();
-
-        // if currently touching screen at correct spot
-        if (placementPoseIsValid && Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        if (isBeginningObjectSpawned == false)
         {
-            PlaceObject();
+            UpdatePlacement();
+            UpdatePlacementIndicator();
+            // if currently touching screen at correct spot
+            if (placementPoseIsValid && Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+            {
+                PlaceObjectBeginning();
+
+            }
         }
+        
+        if (isBeginningObjectSpawned == true && Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            if (touch.phase == TouchPhase.Moved)
+            {
+                transform.position = new Vector3(transform.position.x + touch.deltaPosition.x * objectMovingSpeed, transform.position.y, transform.position.z + touch.deltaPosition.y * objectMovingSpeed);
+            }
+            //touchPosition = touch.position;
+
+            //if (touch.phase == TouchPhase.Began)
+            //{
+            //    Ray ray = Camera.main.ScreenPointToRay(touch.position);
+            //    RaycastHit hitObject;
+            //    if (Physics.Raycast(ray, out hitObject, 5.0f))
+            //    {
+            //        placedObject = hitObject.transform.GetComponent<GameObject>();
+
+            //        MeshRenderer meshRenderer = placedObject.GetComponent<MeshRenderer>();
+            //        meshRenderer.material.color = new Color(1, 0, 0, 1);
+            //        if (hitObject.transform.tag.Contains("bottle"))
+            //        {
+            //            isTouchHolding = true;
+            //        }
+            //    }
+            //}
+            //if (touch.phase == TouchPhase.Ended)
+            //{
+            //    isTouchHolding = false;
+            //}
+            //if (isTouchHolding)
+            //{
+            //    //touchPosition = touch.position;
+            //    placedObject.transform.position = touch.position;
+            //    //placedObject.transform.rotation = touch.rotation;
+            //}
+        }
+        //if (isTouchHolding)
+        //{
+        //    placedObject.transform.position = hitPose.position;
+        //    placedObject.transform.rotation = hitPose.rotation;
+        //}
     }
 
-    private void PlaceObject()
+    private void PlaceObjectBeginning()
     {
-        Instantiate(cabinetToPlace, placementPose.position, placementPose.rotation);
-        Instantiate(shelfToPlace, placementPose.position, placementPose.rotation);
+        // things to place related to cabinet
+        Instantiate(cabinetToPlace, placementPose.position + new Vector3(0.0f, 0.5f, 0.5f), placementPose.rotation);
+        Instantiate(cup, placementPose.position + new Vector3(0.0f, 1.0f, 0.5f), placementPose.rotation);
+
+        // things to place related to shelf
+        Instantiate(shelfToPlace, placementPose.position + new Vector3(0.0f, 0.4f, -0.4f), placementPose.rotation);
+        Instantiate(bottle, placementPose.position + new Vector3(0.0f, 0.8f, -0.4f), placementPose.rotation);
+
+        placementIndicator.SetActive(false);
+        isBeginningObjectSpawned = true; // prevent object from being placed anymore
     }
 
     private void UpdatePlacementIndicator()
@@ -70,6 +134,5 @@ public class ARObjectPlacement : MonoBehaviour
             var cameraBearing = new Vector3(cameraForward.x, 0, cameraForward.z).normalized;
             placementPose.rotation = Quaternion.LookRotation(cameraBearing);
         }
-
     }
 }
