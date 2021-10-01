f3e140bf2cc25858f53930acfde151d0be45e872
Li, Jacob (Agoda) <Jacob.Li@agoda.com>
2021-10-01 11:13:14 -0400

    update tutorial

---
+++ Assets/Scripts/ObjectScripts/TouchLookAt.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchLookAt : MonoBehaviour
+{
+    private Camera arCamera;
+
+    void Start()
+    {
+        arCamera = FindObjectOfType<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var tempTrans = arCamera.transform;
+
+        this.transform.LookAt(tempTrans);
+        this.transform.rotation *= Quaternion.Euler(0, -90, 0);
+    }
+}
