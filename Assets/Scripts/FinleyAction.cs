c6fc5e70a62157d2629f9c613632a2b696befd4b
Yuanqin Fan <fanyuanqin96@gmail.com>
2021-11-01 16:59:06 -0400

    Added main scene

---
+++ Assets/Scripts/FinleyAction.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FinleyAction : MonoBehaviour
+{
+    [SerializeField]
+    private Camera arCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        arCamera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            // raycasting to hit the point
+            Ray ray = arCamera.ScreenPointToRay(touch.position);
+            if (Physics.Raycast(ray, out RaycastHit hitObject))
+            {
+                // touched dot logic
+                if (hitObject.transform.tag == "finley")
+                {
+                    if (IsDoubleTap())
+                    {
+                        SceneManager.LoadScene("main");
+                    }
+                }
+            }
+        }
+    }
+
+    private bool IsDoubleTap()
+    {
+        bool result = false;
+        float MaxTimeWait = 2;
+        float VariancePosition = 2;
+
+        if (Input.touchCount == 1 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+
+            float DeltaTime = Input.GetTouch(0).deltaTime;
+            float DeltaPositionLength = Input.GetTouch(0).deltaPosition.magnitude;
+
+            if (DeltaTime > 0 && DeltaTime < MaxTimeWait && DeltaPositionLength < VariancePosition)
+                result = true;
+        }
+        return result;
+    }
+}
---
+++ Assets/Scripts/MainGM.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainGM : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private string activity1SceneName = "Activity1";
+    private string activity2SceneName = "Activity2";
+    private string activity3SceneName = "Activity3";
+
+    public void loadActivity1Scene()
+    {
+        SceneManager.LoadScene(activity1SceneName);
+    }
+
+    public void loadActivity2Scene()
+    {
+        SceneManager.LoadScene(activity2SceneName);
+    }
+
+    public void loadActivity3Scene()
+    {
+        SceneManager.LoadScene(activity3SceneName);
+    }
+}
